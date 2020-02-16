d4e569796bd4cd2a7d9c70404f7e0dd6d1f47298
yankzsoe <yankzsoe@gmail.com>
2020-02-16 15:05:39 +0700

    Depedency Injection

--- BlazorMovies/Client/Program.cs
+++ BlazorMovies/Client/Program.cs
@@ -4,14 +4,22 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Blazor.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using BlazorMovies.Client.Helpers;
 
 namespace BlazorMovies.Client {
     public class Program {
         public static async Task Main(string[] args) {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            ConfigureServices(builder.Services);
 
             await builder.Build().RunAsync();
+        }
+
+        private static void ConfigureServices(IServiceCollection services) {
+            services.AddSingleton<SingletonServices>();
+            services.AddTransient<TransientServices>();
+            services.AddTransient<IRepository, RepositoryInMemory>();
         }
     }
 }
---
+++ BlazorMovies/Client/Services.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client {
+    public class SingletonServices {
+        public int Value { get; set; }
+    }
+
+    public class TransientServices {
+        public int Value { get; set; }
+    }
+}
