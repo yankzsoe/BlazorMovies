08d8dd4913593e5a0d1affda266497d228353b86
yankzsoe <yankzsoe@gmail.com>
2020-02-18 07:45:34 +0700

    Add custom.css, add group movie, genre and people, add new field Poster and TitleBrief in the movie entities.

--- BlazorMovies/Shared/Entities/Movie.cs
+++ BlazorMovies/Shared/Entities/Movie.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BlazorMovies.Shared.Entities
-{
-    public class Movie
-    {
+namespace BlazorMovies.Shared.Entities {
+    public class Movie {
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
+        public string Poster { get; set; }
+        public string TitleBrief {
+            get {
+                if (string.IsNullOrWhiteSpace(Title)) {
+                    return null;
+                } else if (Title.Length > 60) {
+                    return Title.Substring(0, 60) + "...";
+                } else {
+                    return Title;
+                }
+            }
+        }
     }
 }
