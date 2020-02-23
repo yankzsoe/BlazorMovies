1b9ae38f7888eb3d5c68216b1b8bc05de9cb56ea
yankzsoe <yankzsoe@gmail.com>
2020-02-23 11:12:44 +0700

    EditForm & InputText

---
+++ BlazorMovies/Shared/Entities/Genre.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMovies.Shared.Entities {
+    public class Genre {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
