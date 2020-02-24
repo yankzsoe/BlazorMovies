f683d1acb9e02714848fe2349ba50f5691b76c19
yankzsoe <yankzsoe@gmail.com>
2020-02-25 06:36:00 +0700

    Person Form

---
+++ BlazorMovies/Shared/Entities/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Shared.Entities {
+    public class Person {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public string Picture { get; set; }
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
